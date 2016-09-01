bf74939e97b34cb22a0b85b421801722f43edb90
John Rose <jrose@ggc.edu>
2016-09-01 17:41:13 -0400

    Added message to prompt user for width and height and returned total number of pixels

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
-echo "Enter your first number"
-read first
-echo "Enter your second number"
-read second
-echo "Enter your third number"
-read third
+#echo "Enter your first number"
+#read first
+#echo "Enter your second number"
+#read second
+#echo "Enter your third number"
+#read third
 
-sum=$(( first+second+third ))
+#sum=$(( first + second + third ))
 
-echo "The sum is $sum"
+#echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+ echo "Please enter the width."
+ read width
+ echo "Please enter the height."
+ read height
+ 
+ totalPixels=$(($width * $height))
+ 
+ echo "Total pixels = $totalPixels"
