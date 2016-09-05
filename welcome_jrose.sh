2d94fd53605611ad37374cc1a01160ef87617f90
John Rose <jrose@ggc.edu>
2016-09-05 12:02:48 -0400

    WTF explains it allgit add welcome_jrose.sh!

--- welcome_jrose.sh
+++ welcome_jrose.sh
@@ -5,3 +5,5 @@ echo "Welcome, John Rose!"
 echo "Please enter your birthday."
 
 echo "Please enter the city you live in."
+
+echo "WTF!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
