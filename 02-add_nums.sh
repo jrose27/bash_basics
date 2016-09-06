861b7439b0145393fc6a937698b9b41b2dd35964
John Rose <jrose@ggc.edu>
2016-09-06 16:22:12 -0400

    Added prompt for favorite class

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,7 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo "what is your favorite class this semester"
+read class
+echo "You chose $class as your favorite class."
